848e1b64eee5ba47c66700174eb63240be9a1fd0
KB <baczewska.katarzyna@gmail.com>
2022-06-29 19:07:04 +0200

    zadanie 1 poprawki

--- plyty.py
+++ plyty.py
@@ -2,13 +2,16 @@ import pandas as pd
 
 data = pd.read_html('https://www.officialcharts.com/chart-news/the-best-selling-albums-of-all-time-on-the-official-uk-chart__15551/', header=0)
 df = data[0]
+
 #%%
 #1 Zamień nagłówki kolumn na polskie odpowiedniki: ['TYTUŁ','ARTYSTA','ROK','MAX POZ']
 df.rename(columns={'TITLE':'TYTUŁ','ARTIST':'ARTYSTA','YEAR':'ROK','HIGH POSN':'MAX POZ'},inplace=True)
+
 #%%
 #2 Ilu pojedynczych artystów znajduje się na liście?
 n_artists = df['ARTYSTA'].nunique()
 print(f'na liscie znajduje się : {n_artists} indywidualnych artystów')
+
 #%%
 #3 Które zespoły pojawiają się najczęściej na liście?   
 freq_artists = df['ARTYSTA'].value_counts()
@@ -16,16 +19,16 @@ n = freq_artists.max()
 artists_max = freq_artists.where(freq_artists==n)
 artists_max = artists_max.dropna()
 print(f'Na liscie najczęciej pojawiały sie: {artists_max.index[0]} i {artists_max.index[1]}')
+
 #%%
 #4 Zmień nagłówki kolumn, tak aby każdy z nich rozpoczynał się od wielkiej litery, a pozostałe były wprowadzone małymi literami
 df.columns = df.columns.astype("str")
 df.columns = df.columns.str.lower()
 df.columns = df.columns.str.capitalize()
 
-
 #%%
 #5 Wyrzuć z tabeli kolumnę ‘Max Poz’
-df = df.drop('Max poz',axis=1)
+df.drop('Max poz',axis=1, inplace=True)
 
 #%%
 #6 W którym roku wyszło najwięcej albumów znajdujących się na liście?
